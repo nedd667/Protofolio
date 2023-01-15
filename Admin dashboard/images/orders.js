39e42e40473d1d3b0a386b7a0dbb92505951f174
Nedim <57709682+nedd667@users.noreply.github.com>
2023-01-15 18:03:50 +0100

    Add files via upload

---
+++ Admin dashboard/images/orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "44861",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "LARVENDER KF102 Drone",
+    productNumber: "84851",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "Ruko F11 Pro Drone",
+    productNumber: "12348",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Drone with Camera Drone",
+    productNumber: "34458",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "GPS 4k Drone",
+    productNumber: "74925",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+];
